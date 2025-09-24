f99cebe515abde4072042ab349db99bc2239f45e
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-24 17:14:20 -0300

    Implementando tratamento dos erros.

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,26 @@
+import { ZodError, z } from "zod";
+import { AppError } from "@/utils/AppError";
+import { NextFunction, Request, Response } from "express";
+import { app } from "@/app";
+
+export const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (err instanceof AppError) {
+        return res.status(400).json({
+            message: err.message,
+        });
+    }
+    if (err instanceof ZodError) {
+        return res
+            .status(400)
+            .json({ message: "Validation error", issues: z.treeifyError(err) });
+    }
+
+    return res.status(500).json({
+        message: err.message || "Internal server error",
+    });
+};
