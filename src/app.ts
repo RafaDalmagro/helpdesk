1df58d6d4abc3c7285629f9938dbec6e27c4fa9b
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-10-23 13:29:18 -0300

    feat: instalando e implementando o CORS na aplicação.

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { routes } from "@/routes";
 import { errorHandler } from "@/middlewares/errorHandler";
 import UploadConfig from "./configs/UploadConfig";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/uploads", express.static(UploadConfig.UPLOADS_FOLDER));
