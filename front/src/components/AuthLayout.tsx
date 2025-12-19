785082f8fd5a8298891875db17e7d43d4a3fb79e
RafaDalmagro <rafa.limadalmagro@gmail.com>
2025-12-19 09:13:18 -0300

    refact: Ajusta a responsividade e inclui a s classes corretas para tal.

--- front/src/components/AuthLayout.tsx
+++ front/src/components/AuthLayout.tsx
@@ -1,19 +1,18 @@
 import { Outlet } from "react-router";
 
-import logoSvg from "../assets/logo.svg";
+import NavHeader from "../assets/NavHeader.svg";
 import loginBackground from "../assets/Login_Background.svg";
 
 export function AuthLayout() {
     return (
         <div
-            className="w-screen h-screen bg-cover bg-center bg-no-repeat"
+            className="w-screen h-screen bg-cover bg-center bg-no-repeat pt-8 md:pt-3 overflow-hidden"
             style={{ backgroundImage: `url(${loginBackground})` }}>
-            <main className="flex gap-2 justify-end">
-                <div className="h-screen bg-gray-50 py-12 px-35 mt-3 rounded-tl-xl">
-                    <img src={logoSvg} alt="Logo" className="w-10" />
-                    <span className="text-purple-800 text-xl font-bold content-center">
-                        Help Desk
-                    </span>
+            <main
+                className="flex w-screen h-screen bg-gray-50 py-8 px-6 rounded-t-xl  overflow-y-auto justify-center
+            sm:justify-self-end sm:w-1/2 lg:py-12 lg:px-35 sm:rounded-tl-xl sm:rounded-t-none">
+                <div className="flex flex-col gap-6 content-center w-screen">
+                    <img src={NavHeader} alt="Logo" className="h-10" />
 
                     <Outlet />
                 </div>
