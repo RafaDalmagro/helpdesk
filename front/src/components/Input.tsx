ac827c60982f09f084d0f9d23a9d143df47d07d1
RafaDalmagro <rafa.limadalmagro@gmail.com>
2025-12-19 09:48:10 -0300

    feat: Inclui o componente de input e insere na pagina de SignIn.

---
+++ front/src/components/Input.tsx
@@ -0,0 +1,13 @@
+type Props = React.ComponentProps<"input"> & {
+    legend?: string;
+};
+
+export function Input({ legend, ...rest }: Props) {
+    return (
+        <fieldset className="flex flex-1 border-b border-gray-500">
+            {legend && <legend className="text-xxs uppercase font-bold text-gray-300">{legend}</legend>}
+
+            <input type="text" {...rest} className="text-gray-400 pb-1 w-full outline-none"/>
+        </fieldset>
+    );
+}
--- front/src/pages/SignIn.tsx
+++ front/src/pages/SignIn.tsx
@@ -1,5 +1,28 @@
+import { Input } from "../components/Input";
 
-
-export function SignIn(){
-	return <h1>Sign In</h1>
+export function SignIn() {
+    return (
+        <form className="flex w-full flex-col gap-8 p-6 border-gray-500 border rounded-xl">
+            <div className="">
+                <h1 className="text-lg text-gray-200 font-bold">
+                    Acesse o portal
+                </h1>
+                <p className="text-xs text-gray-300">
+                    Entre usando seu e-mail e senha cadastrados
+                </p>
+            </div>
+            <Input
+                legend="E-mail"
+                required
+                type="email"
+                placeholder="exemplo@mail.com"
+            />
+            <Input
+                legend="Senha"
+                required
+                type="password"
+                placeholder="Digite sua senha"
+            />
+        </form>
+    );
 }
