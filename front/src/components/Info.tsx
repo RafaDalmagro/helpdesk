8b2ee509b7f65b97ac64101d9dece3596bdd2e6a
RafaDalmagro <rafa.limadalmagro@gmail.com>
2025-12-20 19:12:17 -0300

    refact: inclui a exibição em tamanho de telas maiores.

--- front/src/components/Info.tsx
+++ front/src/components/Info.tsx
@@ -1,5 +1,4 @@
 import { Status } from "./Status";
-import { useState } from "react";
 
 type InfoProps = {
     chamado: Chamado;
@@ -8,14 +7,19 @@ type InfoProps = {
 export function Info({ chamado }: InfoProps) {
     return (
         <section className="h-full flex flex-col gap-4">
-            <div className="flex-1 border border-gray-500 rounded-md p-5">
+            <div className="flex flex-col flex-1 gap-1 border border-gray-500 rounded-md p-5">
                 <div className="flex items-center justify-between gap-4">
-                    <span>{chamado.id}</span>
+                    <span className="font-xs text-gray-300 font-bold">
+                        {chamado.id}
+                    </span>
                     <Status status={chamado.status} />
                 </div>
+                <h3 className="font-md font-bold">{chamado.titulo}</h3>
             </div>
 
-            <div className="flex-1 border border-gray-500 rounded-md p-5" />
+            <div className="flex-1 border border-gray-500 rounded-md p-5">
+				
+			</div>
         </section>
     );
 }
