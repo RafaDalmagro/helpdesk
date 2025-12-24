bf070f71c0eda00f528ee3c691f6919d6f4c202f
RafaDalmagro <rafa.limadalmagro@gmail.com>
2025-12-23 21:57:35 -0300

    refact: Cria duas variantes para o button link, uma para o tipo perfil e outra para o tipo sair.

--- front/src/components/ButtonLink.tsx
+++ front/src/components/ButtonLink.tsx
@@ -5,10 +5,19 @@ import techIcon from "../assets/users.svg";
 import clientIcon from "../assets/briefcase-business.svg";
 import serviceIcon from "../assets/wrench.svg";
 import createIcon from "../assets/plus.svg";
+import sairIcon from "../assets/logout.svg";
+import perfilIcon from "../assets/circle-user.svg";
 
 type Props = {
     title: string;
-    variant?: "chamados" | "tecnicos" | "clientes" | "servicos" | "create";
+    variant?:
+        | "chamados"
+        | "tecnicos"
+        | "clientes"
+        | "servicos"
+        | "create"
+        | "perfil"
+        | "sair";
     iconSvg?: string;
     to: string;
 };
@@ -23,6 +32,8 @@ export function ButtonLink({ title, iconSvg, variant, to }: Props) {
         clientes: clientIcon,
         servicos: serviceIcon,
         create: createIcon,
+        perfil: perfilIcon,
+        sair: sairIcon,
     };
 
     const iconSrc = variant ? variantClasses[variant] : iconSvg;
@@ -31,11 +42,26 @@ export function ButtonLink({ title, iconSvg, variant, to }: Props) {
     return (
         <Link
             to={to}
-            className={`flex gap-3 p-3 rounded-lg hover:cursor-pointer hover:opacity-70 transition ease-linear w-full ${
-                isActive ? "bg-purple-800" : "bg-inherit"
-            }`}>
+            className={
+                variant !== "sair" && variant !== "perfil"
+                    ? `flex gap-3 p-3 rounded-lg hover:cursor-pointer hover:opacity-70 transition ease-linear w-full ${
+                          isActive ? "bg-purple-800" : "bg-inherit"
+                      }`
+                    : `flex gap-2 p-0 rounded-lg hover:cursor-pointer hover:opacity-70 transition ease-linear w-full ${
+                          isActive ? "bg-purple-800" : "bg-inherit"
+                      }`
+            }>
             {iconSrc && <img src={iconSrc} alt={iconAlt} />}
-            <span className="text-sm text-gray-600">{title}</span>
+            <span
+                className={
+                    variant === "sair"
+                        ? "text-md text-red"
+                        : variant === "perfil"
+                        ? "text-md text-gray-500"
+                        : "text-sm text-gray-600"
+                }>
+                {title}
+            </span>
         </Link>
     );
 }
--- front/src/components/CardPerfil.tsx
+++ front/src/components/CardPerfil.tsx
@@ -1,18 +1,13 @@
-import userSvg from "../assets/circle-user.svg";
-import logoutSvg from "../assets/logout.svg";
+import { ButtonLink } from "./ButtonLink";
 
 export function CardPerfil() {
     return (
         <div className="bg-gray-100 px-5 py-4 flex flex-col gap-4 rounded-lg border border-gray-300 z-10 w-35 absolute left-2 top-2 md:left-2 md:bottom-2 md:top-auto">
-            <span className="capitalize text-gray-400 text-xxs">Opções</span>
-            <div className="flex gap-2">
-                <img src={userSvg} alt="Perfil" />
-                <span className="text-gray-500 text-md">Perfil</span>
-            </div>
-            <div className="flex gap-2">
-                <img src={logoutSvg} alt="Sair" />
-                <span className="text-red text-md">Sair</span>
-            </div>
+            <span className="uppercase font-bold text-gray-400 text-xxs">
+                Opções
+            </span>
+            <ButtonLink title="Perfil" variant="perfil" to="/perfil" />
+            <ButtonLink title="Sair" variant="sair" to="/login" />
         </div>
     );
 }
