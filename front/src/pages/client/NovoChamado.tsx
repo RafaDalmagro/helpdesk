eaa8327073ab9b2400258d06ed6188a5ec6a6629
RafaDalmagro <rafa.limadalmagro@gmail.com>
2026-01-07 09:59:05 -0300

    refact: ajusta o alinhamento do componente da pagina de novo chamado para ficar a esquerda da tela.

--- front/src/pages/client/NovoChamado.tsx
+++ front/src/pages/client/NovoChamado.tsx
@@ -113,94 +113,86 @@ export function NovoChamado() {
     }
 
     return (
-        <div className="flex w-full h-full bg-gray-600 rounded-t-xl md:rounded-t-none md:rounded-tl-xl">
-            <section className=" flex flex-1 flex-col gap-4 overflow-y-auto box-border px-6 pb-6 pt-7 md:px-12 md:pb-12 md:pt-13 mx-auto max-w-4xl">
-                <h2 className="text-2xl text-purple-800 font-bold">
-                    Novo Chamado
-                </h2>
-                <form
-                    onSubmit={onSubmit}
-                    className="flex flex-col gap-4 md:flex-row md:gap-6">
-                    <div className="flex flex-col gap-5 p-5 border border-gray-500 rounded-lg md:p-8">
+        <section className="flex flex-1 bg-gray-600 rounded-t-xl md:rounded-t-none md:rounded-tl-xl flex-col gap-4 overflow-y-auto box-border px-6 pb-6 pt-7 md:px-12 md:pb-12 md:pt-13">
+            <h2 className="text-2xl text-purple-800 font-bold">Novo Chamado</h2>
+            <form
+                onSubmit={onSubmit}
+                className="flex flex-col gap-4 md:flex-row md:gap-6">
+                <div className="flex flex-col gap-5 p-5 border border-gray-500 rounded-lg md:p-8">
+                    <div>
+                        <h3 className="text-md text-gray-200 font-bold">
+                            Informações
+                        </h3>
+                        <p className="text-xs text-gray-300">
+                            Configure os dias e horários em que você está
+                            disponível para atender chamados
+                        </p>
+                    </div>
+                    <div className="flex flex-col gap-4">
                         <div>
-                            <h3 className="text-md text-gray-200 font-bold">
-                                Informações
-                            </h3>
-                            <p className="text-xs text-gray-300">
-                                Configure os dias e horários em que você está
-                                disponível para atender chamados
-                            </p>
+                            <Input
+                                required
+                                label="Título"
+                                placeholder="Digite um título para o chamado"
+                                className="py-2"
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
                         </div>
-                        <div className="flex flex-col gap-4">
-                            <div>
-                                <Input
-                                    required
-                                    label="Título"
-                                    placeholder="Digite um título para o chamado"
-                                    className="py-2"
-                                    onChange={(e) => setTitle(e.target.value)}
-                                />
-                            </div>
-                            <div>
-                                <TextArea
-                                    required
-                                    label="Descrição"
-                                    placeholder="Digite um título para o chamado"
-                                    rows={5}
-                                    onChange={(e) =>
-                                        setDescription(e.target.value)
-                                    }
-                                />
-                            </div>
-                            <div>
-                                <Select
-                                    required
-                                    label="Categoria de serviço"
-                                    options={categoryOptions}
-                                    onChange={(e) =>
-                                        setCategoryId(e.target.value)
-                                    }
-                                />
-                            </div>
+                        <div>
+                            <TextArea
+                                required
+                                label="Descrição"
+                                placeholder="Digite um título para o chamado"
+                                rows={5}
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <Select
+                                required
+                                label="Categoria de serviço"
+                                options={categoryOptions}
+                                onChange={(e) => setCategoryId(e.target.value)}
+                            />
                         </div>
                     </div>
-                    <div className="flex flex-col gap-5 p-5 border border-gray-500 rounded-lg md:h-fit">
-                        <div>
-                            <h3 className="text-md text-gray-200 font-bold">
-                                Resumo
-                            </h3>
-                            <p className="text-xs text-gray-300">
-                                Valores e detalhes
-                            </p>
-                        </div>
-                        <div className="flex flex-col gap-4">
-                            <div className="flex flex-col gap-0.5">
-                                <label className="text-xs text-gray-400 font-bold">
-                                    Categoria de serviço
-                                </label>
-                                <span className="text-xs text-gray-200">
-                                    Erro de rede
-                                </span>
-                            </div>
-                            <div className="flex flex-col gap-0.5">
-                                <label className="text-xs text-gray-400 font-bold">
-                                    Custo inicial
-                                </label>
-                                <span className="text-gray-200 text-lg font-bold">
-                                    <small className="text-xs">R$ </small>200,00
-                                </span>
-                            </div>
-                        </div>
-                        <div>
-                            <p className="text-xs text-gray-300">
-                                O chamado será automaticamente atribuído a um
-                                técnico disponível
-                            </p>
-                        </div>
-                        <Button type="submit">Criar chamado</Button>
+                </div>
+                <div className="flex flex-col gap-5 p-5 border border-gray-500 rounded-lg md:h-fit">
+                    <div>
+                        <h3 className="text-md text-gray-200 font-bold">
+                            Resumo
+                        </h3>
+                        <p className="text-xs text-gray-300">
+                            Valores e detalhes
+                        </p>
                     </div>
-                </form>
-            </section>
-        </div>
+                    <div className="flex flex-col gap-4">
+                        <div className="flex flex-col gap-0.5">
+                            <label className="text-xs text-gray-400 font-bold">
+                                Categoria de serviço
+                            </label>
+                            <span className="text-xs text-gray-200">
+                                Erro de rede
+                            </span>
+                        </div>
+                        <div className="flex flex-col gap-0.5">
+                            <label className="text-xs text-gray-400 font-bold">
+                                Custo inicial
+                            </label>
+                            <span className="text-gray-200 text-lg font-bold">
+                                <small className="text-xs">R$ </small>200,00
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <p className="text-xs text-gray-300">
+                            O chamado será automaticamente atribuído a um
+                            técnico disponível
+                        </p>
+                    </div>
+                    <Button type="submit">Criar chamado</Button>
+                </div>
+            </form>
+        </section>
     );
 }
