9ed3a531b05619de42ded51d2faeb24907d66a0b
RafaDalmagro <rafa.limadalmagro@gmail.com>
2026-01-19 16:09:06 -0300

    feat: implementando configurações para o deploy da aplicação.

--- back/src/env.ts
+++ back/src/env.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 
 const envSchema = z.object({
     PORT: z.coerce.number().default(3000),
+    JWT_SECRET: z.string(),
+    DATABASE_URL: z.url(),
 });
 
 export const env = envSchema.parse(process.env);
