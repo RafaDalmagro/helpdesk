a995dcab16d14910b923771bbdf46461d6a9a63f
RafaDalmagro <rafa.limadalmagro@gmail.com>
2026-01-20 10:21:00 -0300

    refact: ajusta a exibição do menu em telas mobila via toggle nas rotas de client e tech.

--- front/src/components/Menu.tsx
+++ front/src/components/Menu.tsx
@@ -41,7 +41,8 @@ export function Menu({ className = "hidden" }: MenuProps) {
             );
         case "tech":
             return (
-                <nav className="hidden md:flex md:flex-col md:gap-1 border-t border-b border-gray-200 w-full py-5 px-4 md:flex-10">
+                <nav
+                    className={`${className} md:flex md:flex-col md:gap-1 border-t border-b border-gray-200 w-full py-5 px-4 md:flex-10`}>
                     <ButtonLink
                         title="Meus Chamados"
                         variant="chamados"
@@ -51,7 +52,8 @@ export function Menu({ className = "hidden" }: MenuProps) {
             );
         case "client":
             return (
-                <nav className="hidden md:flex md:flex-col md:gap-1 border-t border-b border-gray-200 w-full py-5 px-4 md:flex-10">
+                <nav
+                    className={`${className} md:flex md:flex-col md:gap-1 border-t border-b border-gray-200 w-full py-5 px-4 md:flex-10`}>
                     <ButtonLink
                         title="Meus Chamados"
                         variant="chamados"
