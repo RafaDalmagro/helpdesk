fb1281ddbb032150af9d486edcb0c8be6ffac6e0
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2026-01-20 22:43:17 -0300

    refact: ajusta a posiçao do ocmponente CardPerfil.

--- front/src/components/CardPerfil.tsx
+++ front/src/components/CardPerfil.tsx
@@ -21,7 +21,7 @@ export function CardPerfil() {
     const user = getUserNameEmail();
 
     return (
-        <div className="absolute bg-gray-100 px-3 py-2 md:px-5 md:py-4 flex flex-col gap-2 md:gap-4 rounded-md md:rounded-lg border border-gray-300 z-10 md:w-35 -right-5 -bottom-31 md:-right-11/12 md:-bottom-3/12">
+        <div className="absolute bg-gray-100 px-3 py-2 md:px-5 md:py-4 flex flex-col gap-2 md:gap-4 rounded-md md:rounded-lg border border-gray-300 z-10 md:w-35 -right-5 -bottom-31 md:-right-11/12 md:-bottom-6/12">
             <span className="uppercase font-bold text-gray-400 text-xxs">
                 Opções
             </span>
