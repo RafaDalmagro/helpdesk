799478fd84dba6120e494e16e5748ecd94b324c5
RafaDalmagro <rafa.limadalmagro@gmail.com>
2026-01-25 09:50:27 -0300

    feat: implementa o componente para selecionar os horarios do técnico.

---
+++ front/src/components/ScheduleSelector.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import x from "../assets/x-white.svg";
+
+const PERIODS = [
+    {
+        label: "Manhã",
+        times: ["07:00", "08:00", "09:00", "10:00", "11:00", "12:00"],
+    },
+    {
+        label: "Tarde",
+        times: ["13:00", "14:00", "15:00", "16:00", "17:00", "18:00"],
+    },
+    { label: "Noite", times: ["19:00", "20:00", "21:00", "22:00", "23:00"] },
+];
+
+type ScheduleSelectorProps = {
+    onChange?: (selectedSlots: string[]) => void;
+    className?: string;
+};
+
+export function ScheduleSelector({
+    onChange,
+    className,
+}: ScheduleSelectorProps) {
+    const [selectedSlots, setSelectedSlots] = useState<string[]>([]);
+
+    const handleToggleSlot = (time: string) => {
+        setSelectedSlots((prevSlots) => {
+            if (prevSlots.includes(time)) {
+                const newSlots = prevSlots.filter((t) => t !== time);
+                if (onChange) onChange(newSlots);
+                return newSlots;
+            }
+
+            const newSlots = [...prevSlots, time];
+            if (onChange) onChange(newSlots); // Avisa o pai
+            return newSlots;
+        });
+    };
+
+    return (
+        <div
+            className={`rounded-xl p-6 flex flex-col md:w-fit gap-6 border border-gray-500 ${className}`}>
+            <div className="flex flex-col gap-1">
+                <h2 className="text-md font-bold text-gray-200">
+                    Horários de atendimento
+                </h2>
+                <p className="text-gray-300 text-xs">
+                    Selecione os horários de disponibilidade do técnico
+                </p>
+            </div>
+            <div className="flex flex-col gap-5">
+                {PERIODS.map((period) => (
+                    <div key={period.label} className="flex flex-col gap-2">
+                        <h3 className="text-xxs font-bold text-gray-300 uppercase">
+                            {period.label}
+                        </h3>
+
+                        <div className="flex flex-wrap gap-2">
+                            {period.times.map((time) => {
+                                const isSelected = selectedSlots.includes(time);
+
+                                return (
+                                    <button
+                                        key={time}
+                                        type="button"
+                                        onClick={() => handleToggleSlot(time)}
+                                        className={`p-1.5 rounded-full font-bold text-xs transition-all duration-200 border flex items-center hover:cursor-pointer ${
+                                            isSelected
+                                                ? "bg-purple-400 text-gray-600 border-purple-400 hover:opacity-80"
+                                                : "bg-gray-600 text-gray-200 border-gray-300 hover:opacity-80"
+                                        } `}>
+                                        {time}
+                                        {isSelected && (
+                                            <img src={x} alt="Remove" />
+                                        )}
+                                    </button>
+                                );
+                            })}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
--- front/src/pages/admin/Tecnico.tsx
+++ front/src/pages/admin/Tecnico.tsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import { useTechs } from "../../hooks/useTechs";
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
+
+import { ScheduleSelector } from "../../components/ScheduleSelector";
+import { Button } from "../../components/Button";
+import { Voltar } from "../../components/VoltarLink";
+import { UserInitials } from "../../components/UserInitials";
 
 export function Tecnico() {
     const { id } = useParams();
+    const navigate = useNavigate();
+
     const { loading, error, fetchTechById } = useTechs();
+
     const [tech, setTech] = useState<UserTechDetail | null>(null);
     const [isFetching, setIsFetching] = useState(true);
 
@@ -12,7 +20,9 @@ export function Tecnico() {
         async function loadTech() {
             if (!id) return;
             setIsFetching(true);
+
             const techData = await fetchTechById(id);
+
             setTech(techData);
             setIsFetching(false);
         }
@@ -20,9 +30,41 @@ export function Tecnico() {
         loadTech();
     }, [id]);
 
+    function handleCancelar() {
+        navigate(-1);
+    }
+
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("Form submitted");
+    };
+
     return (
-        <section className="bg-gray-600 flex flex-1 flex-col rounded-t-xl md:rounded-t-none md:rounded-tl-xl overflow-y-auto box-border gap-4 md:gap-6 px-6 pb-6 pt-7 md:px-12 md:pb-12 md:pt-13 relative">
-            <h2 className="text-2xl text-purple-800 font-bold mb-4">Técnico</h2>
+        <form
+            className="bg-gray-600 flex flex-1 flex-col rounded-t-xl md:rounded-t-none md:rounded-tl-xl overflow-y-auto box-border gap-4 md:gap-6 px-6 pb-6 pt-7 md:px-12 md:pb-12 md:pt-13 relative md:items-center"
+            onSubmit={onSubmit}>
+            <header className="flex flex-col md:flex-row gap-4 justify-between max-w-4xl w-full">
+                <div className="flex flex-col gap-1">
+                    <Voltar title="Voltar" to="/chamados" />
+                    <h2 className="text-xl text-purple-800 font-bold">
+                        Perfil de técnico
+                    </h2>
+                </div>
+                <div className="flex gap-2 items-center">
+                    <Button
+                        onClick={handleCancelar}
+                        variant="primary"
+                        buttonName="Cancelar"
+                        className="w-full md:h-fit px-4 md:w-fit"
+                    />
+                    <Button
+                        variant="default"
+                        buttonName="Salvar"
+                        type="submit"
+                        className="w-full md:h-fit px-4 md:w-fit"
+                    />
+                </div>
+            </header>
 
             {isFetching ? (
                 <div className="p-4 text-center text-gray-400">
@@ -31,15 +73,47 @@ export function Tecnico() {
             ) : error ? (
                 <div className="p-4 text-center text-red-400">{error}</div>
             ) : tech ? (
-                <div className="overflow-x-auto border border-gray-500 rounded-xl">
-                    {/* Adicionar conteúdo do técnico aqui */}
-                    <pre>{JSON.stringify(tech, null, 2)}</pre>
+                <div className="overflow-x-auto flex flex-col md:flex-row gap-4 md:gap-6 justify-center max-w-4xl w-full">
+                    <div className="border flex flex-col h-fit gap-5 md:gap-6 border-gray-500 rounded-xl p-5 md:p-6 flex-1">
+                        <div className="flex flex-col gap-1">
+                            <h3 className="text-md text-gray-200 font-bold">
+                                Dados pessoais
+                            </h3>
+                            <span className="text-xs text-gray-300">
+                                Defina as informações do perfil técnico
+                            </span>
+                        </div>
+                        <UserInitials
+                            name={tech.name}
+                            variant="simple"
+                            className="size-12 text-sm"
+                        />
+                        <div className="flex flex-col gap-4">
+                            <div className="flex flex-col">
+                                <label className="text-xxs text-gray-300 uppercase font-bold">
+                                    Name
+                                </label>
+                                <span className="text-md text-gray-200 border-b border-gray-500 py-2">
+                                    {tech.name}
+                                </span>
+                            </div>
+                            <div className="flex flex-col">
+                                <label className="text-xxs text-gray-300 uppercase font-bold">
+                                    E-mail
+                                </label>
+                                <span className="text-md text-gray-200 border-b border-gray-500 py-2">
+                                    {tech.email}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <ScheduleSelector className="flex-2" />
                 </div>
             ) : (
                 <div className="p-4 text-center text-gray-400">
                     Técnico não encontrado.
                 </div>
             )}
-        </section>
+        </form>
     );
 }
